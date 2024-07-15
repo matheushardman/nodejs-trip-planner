3460312bc4e4aa56699132471ec7994e4cfb5ebe
Matheus Hardman <matheus.hardman@gmail.com>
2024-07-14 23:16:46 -0300

    Implements new participant invite

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ import { getActivities } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
 import { getLinks } from "./routes/get-links";
 import { getParticipants } from "./routes/get-participants";
+import { createInvite } from "./routes/create-invite";
 
 const app = fastify()
 
@@ -27,6 +28,7 @@ app.register(getActivities)
 app.register(createLink)
 app.register(getLinks)
 app.register(getParticipants)
+app.register(createInvite)
 
 
 app.listen({ port: 3333 }).then(() => {

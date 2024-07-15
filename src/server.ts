f28e8e0ff813226aa538095b41739ebfd5c8fb6a
Matheus Hardman <matheus.hardman@gmail.com>
2024-07-14 23:08:29 -0300

    Implements list of participants

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ import { createActivity } from "./routes/create-activity";
 import { getActivities } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
 import { getLinks } from "./routes/get-links";
+import { getParticipants } from "./routes/get-participants";
 
 const app = fastify()
 
@@ -25,6 +26,7 @@ app.register(createActivity)
 app.register(getActivities)
 app.register(createLink)
 app.register(getLinks)
+app.register(getParticipants)
 
 
 app.listen({ port: 3333 }).then(() => {

20948b3036cdeff9b466b309b250a94131dba168
Matheus Hardman <matheus.hardman@gmail.com>
2024-07-14 22:50:11 -0300

    Implements create and list activities

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import { createTrip } from "./routes/create-trip";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { confirmTrip } from "./routes/confirm-trip";
 import { confirmParticipant } from "./routes/confirm-participant";
+import { createActivity } from "./routes/create-activity";
+import { getActivities } from "./routes/get-activities";
 
 const app = fastify()
 
@@ -17,6 +19,8 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createTrip)
 app.register(confirmTrip)
 app.register(confirmParticipant)
+app.register(createActivity)
+app.register(getActivities)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('Server running!')
